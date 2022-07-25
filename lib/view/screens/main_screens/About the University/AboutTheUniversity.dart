5477cff4e01b8528507888d3498e6c8cae919b1c
mo7amed777 <mohamed010279316@gmail.com>
2022-07-25 13:43:58 +0300

    edit AboutTheUniversity

--- lib/view/screens/main_screens/About the University/AboutTheUniversity.dart
+++ lib/view/screens/main_screens/About the University/AboutTheUniversity.dart
@@ -23,14 +23,39 @@ class _AboutTheUniversityState extends State<AboutTheUniversity> {
         children: [
           Stack(
             children: [
-              MyListTile(
-                title: 'About the University',
-                subtitle:
-                    'Being the only university in the Northern Borders Region, Northern Border University enjoys a unique position in higher education in Saudi Arabia, which is why it holds the name of this precious region. The university plans are inspired by the region\'s rich values, culture, history, and location. We also ensure that our Strategic Plan 2020-2025 goes along with the region\'s rich natural resources, Vision 2030, and with the New University System, guided by the strategic priorities of the Northern Borders Region\'s Emirate.',
+              Card(
+                elevation: 5.0,
+                margin: EdgeInsets.all(8.0),
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    bottom: 32.0,
+                    right: 8.0,
+                    left: 8.0,
+                    top: 8.0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 4),
+                        child: Text('About the University',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: AppTheme.lightText,
+                                fontSize: 16)),
+                      ),
+                      Text(
+                        'Being the only university in the Northern Borders Region, Northern Border University enjoys a unique position in higher education in Saudi Arabia, which is why it holds the name of this precious region. The university plans are inspired by the region\'s rich values, culture, history, and location. We also ensure that our Strategic Plan 2020-2025 goes along with the region\'s rich natural resources, Vision 2030, and with the New University System, guided by the strategic priorities of the Northern Borders Region\'s Emirate.',
+                        style:
+                            TextStyle(fontWeight: FontWeight.w500, height: 1.5),
+                      ),
+                    ],
+                  ),
+                ),
               ),
               Positioned(
-                right: 4.0,
-                bottom: 4.0,
+                right: 12.0,
+                bottom: 12.0,
                 child: InkWell(
                   onTap: () => Get.to(MoreAboutTheUniversity()),
                   child: Row(
@@ -61,27 +86,23 @@ class _AboutTheUniversityState extends State<AboutTheUniversity> {
                   fontSize: 20,
                 )),
           ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height *
-                1.5, // depending on itemCount
-            child: Timeline(
-              position: TimelinePosition.Left,
-              physics: NeverScrollableScrollPhysics(),
-              iconSize: 25,
-              lineColor: AppTheme.lightText,
-              shrinkWrap: true,
-              //if itemCount increases, u also need to increase the height of the timeline
-              children: [
-                timelineModel('1402 AH',
-                    'The Middle College for Girls was established and served as the foundation of this current edifice.'),
-                timelineModel('1408 AH',
-                    'The Middle College for Girls was turned into The Teachers\' College, awarding Bachelor\'s degrees to its students for the first time.'),
-                timelineModel('1426 AH',
-                    'The College of Science in Arar and  Rafha\'s Community College were established to be two branches of King Abdulaziz University.'),
-                timelineModel('1428 AH',
-                    "During his historic visit to the Northern Border Region, the Custodian of the Two Holy Mosques, King Abdullah bin Abdulaziz (may Allah have mercy on him), announced The Royal Decree to establish Northern Borders University. It merged The Teachers' College with the two branches of King Abdulaziz University, both in Arar and Rafha. The establishment of Northern Borders University was immediately followed by establishing several new colleges. These colleges were; The College of Medicine, Pharmacy, Nursing, Applied Medical Sciences, Engineering, Computer Science, Business Administration, and Community College, all of which were distributed around its main campus in Arar. Alongside the three branches of the university in Rafha, Tarif, and Al Uwayqila. The emerging university has undergone a period of rapid development. It has been a significant part of the transformation of cultural, social, and economic progress in the region"),
-              ],
-            ),
+          Timeline(
+            position: TimelinePosition.Left,
+            physics: NeverScrollableScrollPhysics(),
+            iconSize: 25,
+            lineColor: AppTheme.lightText,
+            shrinkWrap: true,
+            //if itemCount increases, u also need to increase the height of the timeline
+            children: [
+              timelineModel('1402 AH',
+                  'The Middle College for Girls was established and served as the foundation of this current edifice.'),
+              timelineModel('1408 AH',
+                  'The Middle College for Girls was turned into The Teachers\' College, awarding Bachelor\'s degrees to its students for the first time.'),
+              timelineModel('1426 AH',
+                  'The College of Science in Arar and  Rafha\'s Community College were established to be two branches of King Abdulaziz University.'),
+              timelineModel('1428 AH',
+                  "During his historic visit to the Northern Border Region, the Custodian of the Two Holy Mosques, King Abdullah bin Abdulaziz (may Allah have mercy on him), announced The Royal Decree to establish Northern Borders University. It merged The Teachers' College with the two branches of King Abdulaziz University, both in Arar and Rafha. The establishment of Northern Borders University was immediately followed by establishing several new colleges. These colleges were; The College of Medicine, Pharmacy, Nursing, Applied Medical Sciences, Engineering, Computer Science, Business Administration, and Community College, all of which were distributed around its main campus in Arar. Alongside the three branches of the university in Rafha, Tarif, and Al Uwayqila. The emerging university has undergone a period of rapid development. It has been a significant part of the transformation of cultural, social, and economic progress in the region"),
+            ],
           ),
         ],
       ),
@@ -105,7 +126,8 @@ TimelineModel timelineModel(String title, String subtitle) {
                         color: AppTheme.lightText,
                         fontSize: 20)),
               ),
-              Text(subtitle, style: TextStyle(fontSize: 16)),
+              Text(subtitle,
+                  style: TextStyle(fontSize: 16, overflow: TextOverflow.fade)),
             ],
           ),
         ),
